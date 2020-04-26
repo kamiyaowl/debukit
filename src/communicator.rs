6e9ab65954354d7351e359f8c77e06395c1498b9
K <kamiyaowl@gmail.com>
2020-04-26 13:51:19 +0900

    最低限のhttp getを実装

---
+++ src/communicator.rs
@@ -0,0 +1,15 @@
+extern crate reqwest;
+
+
+pub fn get_blocking<T: reqwest::IntoUrl>(url: T) -> Result<String, Box<dyn std::error::Error>> {
+    let res: reqwest::blocking::Response = reqwest::blocking::get(url)?;
+    let text: String = res.text()?;
+    Ok(text)
+}
+
+#[test]
+fn test_get_blocking() -> Result<(), Box<dyn std::error::Error>> {
+    let text = get_blocking("https://kamiyaowl.github.io/")?;
+    println!("{}", text);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use debukit::communicator;
+
+fn main() -> Result<(), Box<dyn std::error::Error> {
+    let test_url = "https://kamiyaowl.github.io/";
+    let htmlText = communicator::get_blocking(test_url);
 }
